53c786b125c577e71f90fb0fd11d72a84b0f6f22
arafat-sabbir <arafatshabbir8@gmail.com>
2024-01-23 01:20:11 +0600

    Initialized the Project And Added Navbar

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            This si fro
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import routes from './Routes/Routes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'react-hot-toast'
+
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routes}></RouterProvider>
+      <Toaster />
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
